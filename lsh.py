02df91a08ffcab5fab831433600bad57bf703489
gregbkatz <gregbkatz@gmail.com>
2019-05-27 14:32:54 -0700

    Clean up feature vector generation code

--- lsh.py
+++ lsh.py
@@ -1,6 +1,6 @@
 import numpy as np
 import glob
-from scipy import misc
+from imageio import imread
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
@@ -11,15 +11,32 @@ from skimage.transform import resize
 from skimage.color import rgb2gray, gray2rgb
 import time
 import os
+import shutil
 
 IMSIZE = (224, 224)
-IMAGE_DIR = "./imnet-100"
-# IMAGE_DIR = "./imnet-val"
+BASE_DIR = "./imnet-100"
+# BASE_DIR = "./imnet-val"
+NMAX = -1
 
-def pickle_write_concat_file(fname, im_fnames, all_vecs):
-  concat_data = {"fnames":im_fnames, "all_vecs": all_vecs}
-  with open(fname + ".p", 'wb') as f:
-    pickle.dump(concat_data, f)
+def mkdir_safe(path):
+  if not os.path.exists(path):
+    os.mkdir(path)
+
+def mkdir_clean(path):
+  if os.path.exists(path):
+    shutil.rmtree(path)
+  os.mkdir(path)
+
+def feature_fname_get(feature_name):
+  img_fnames = get_img_fnames()
+  N = len(img_fnames)
+  return os.path.join(BASE_DIR, feature_name + "-" + str(len(img_fnames)) + ".p")
+
+def pickle_write_concat_file(base_dir, feature_name, img_fnames, all_vecs):
+  concat_data = {"fnames":img_fnames, "all_vecs": all_vecs}
+  fname = feature_fname_get(feature_name)
+  with open(fname, 'wb') as f:
+    pickle.dump(concat_data, f, pickle.HIGHEST_PROTOCOL)
 
 def plot_features(image, feature_image):
   plt.subplot(211)
@@ -60,75 +77,134 @@ def per_color_avg(im):
 def first_pixel(im):
   return im[0,0,:]
 
-def im2vectors(im_fnames):
-  for i, fname in enumerate(im_fnames):
+def img_dir_from_base_dir(base_dir):
+  return os.path.join(base_dir, "imgs")
+
+def raw_dir_from_base_dir(base_dir):
+  return os.path.join(base_dir, "imgs_raw_npy")
+
+def features_dir_from_base_dir(base_dir):
+  return os.path.join(base_dir, "feature_vectors")
+
+def raw_name_from_img_name(img_fname):
+  path, file = os.path.split(img_fname)
+  file_no_ext, ext = os.path.splitext(file)
+  base_dir, _ = os.path.split(path)
+  raw_dir = raw_dir_from_base_dir(base_dir)
+  return os.path.join(raw_dir, file_no_ext + '.npy')
+
+def features_name_from_img_name(img_fname):
+  path, file = os.path.split(img_fname)
+  file_no_ext, ext = os.path.splitext(file)
+  base_dir, _ = os.path.split(path)
+  features_dir = features_dir_from_base_dir(base_dir)
+  return os.path.join(features_dir, file_no_ext + '.p')
+
+def get_img_fnames():
+  img_dir = img_dir_from_base_dir(BASE_DIR)
+  img_pattern = os.path.join(img_dir, '*.JPEG')
+  img_fnames = sorted(glob.glob(img_pattern))
+  if NMAX > 0:
+    img_fnames = img_fnames[:NMAX]
+  return img_fnames
+
+def standardize_img(img):
+  img = resize(img, IMSIZE, anti_aliasing=True)
+  if (img.ndim == 2):
+    img = gray2rgb(img)
+
+  if img.shape[2] != 3:
+    pdb.set_trace()
+
+  return img
+
+def imgs2npy():
+  img_fnames = get_img_fnames()
+  N = len(img_fnames)
+
+  raw_dir = raw_dir_from_base_dir(BASE_DIR)
+  mkdir_clean(raw_dir)
+  mkdir_safe(raw_dir)
+
+  for i, img_fname in enumerate(img_fnames):
+    print("imgs2npy ", BASE_DIR, i, N)
+
+    raw_fname = raw_name_from_img_name(img_fname)
+    if os.path.isfile(raw_fname):
+      continue
+    img = imread(img_fname)
+    np.save(raw_fname, img, allow_pickle=False)
+
+
+def npy2features():
+  img_fnames = get_img_fnames()
+  N = len(img_fnames)
+
+  features_dir = features_dir_from_base_dir(BASE_DIR)
+  mkdir_clean(features_dir)
+  mkdir_safe(features_dir)
+
+  for i, img_fname in enumerate(img_fnames):
     t0 = time.time()
-    print(i, len(im_fnames))
-    vec_fname = fname + ".p"
-    # if os.path.isfile(vec_fname):
-      # continue
-    img = misc.imread(fname)
-    img = resize(img, IMSIZE, anti_aliasing=True)
-    if (img.ndim == 2):
-      img = gray2rgb(img)
+    print("npy2features ", BASE_DIR, i, N)
 
-
+    raw_fname = raw_name_from_img_name(img_fname)
+    features_fname = features_name_from_img_name(img_fname)
+    if os.path.isfile(features_fname):
+      continue
+    img = np.load(raw_fname)
+    img = standardize_img(img)
     # plt.imshow(im)
     # plt.show()
-    data = {}
-    t1 = time.time()
-    # data['per_color_avg'] = per_color_avg(img)
-    t2 = time.time()
-    data['hog'] = hog_get(img)
-    t3 = time.time()
-    # data['daisy'] = daisy_get(img)
-    t4 = time.time()
-    data['color_hist'] = color_hist(img)
-    t5 = time.time()
-    data['first_pixel'] = first_pixel(img)
-    # print(t2-t1, t3-t2, t4-t3, t5-t4)
-    with open(vec_fname, 'wb') as f:
-      pickle.dump(data, f)
-    t6 = time.time()
-    print(t6 - t0)
 
-def concat_vectors(im_fnames):
-  vec_fnames = sorted(glob.glob(IMAGE_DIR + "/*.p"))
-  for i, fname in enumerate(vec_fnames):
-    t1 = time.time()
-    with open(fname, 'rb') as f:
+    data = {}
+    # data['per_color_avg'] = per_color_avg(img)
+    data['hog'] = hog_get(img)
+    # data['daisy'] = daisy_get(img)
+    data['color_hist'] = color_hist(img)
+    # data['first_pixel'] = first_pixel(img)
+    with open(features_fname + '.p', 'wb') as f:
+      pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+def concat_features():
+  img_fnames = get_img_fnames()
+  N = len(img_fnames)
+
+  concatenated = {}
+  for i, img_fname in enumerate(img_fnames):
+    print("concat_features", BASE_DIR, i, N)
+
+    features_fname = features_name_from_img_name(img_fname)
+
+    with open(features_fname + '.p', 'rb') as f:
       curr = pickle.load(f)
 
+    # Initialize matrices
     if i == 0:
-      # pc_avgs = np.zeros((len(vec_fnames), len(curr['per_color_avg'])))
-      hogs = np.zeros((len(vec_fnames), len(curr['hog'])))
-      # daisy = np.zeros((len(vec_fnames), len(curr['daisy'])))
-      color_hists = np.zeros((len(vec_fnames), len(curr['color_hist'])))
-      first_pixels = np.zeros((len(vec_fnames), len(curr['first_pixel'])))
+      for feature_name, value in curr.items():
+        concatenated[feature_name] = np.zeros((N, len(value)))
 
-    # pc_avgs[i,:] = curr['per_color_avg']
-    hogs[i,:] = curr['hog']
-    # daisy[i,:] = curr['daisy']
-    color_hists[i,:] = curr['color_hist']
-    first_pixels[i,:] = curr['first_pixel']
+    # Copy data into row
+    for feature_name, value in curr.items():
+      concatenated[feature_name][i,:] = value
 
-    t2 = time.time()
-    print(i, len(vec_fnames), t2 - t1)
+  # Write concatenated data to file
+  for feature_name, value in concatenated.items():
+    pickle_write_concat_file(BASE_DIR, feature_name, img_fnames, value)
 
 
-  # pickle_write_concat_file(IMAGE_DIR + "_pc_avg", im_fnames, pc_avgs)
-  pickle_write_concat_file(IMAGE_DIR + "_hog", im_fnames, hogs)
-  # pickle_write_concat_file(IMAGE_DIR + "_daisy", im_fnames, daisy)
-  pickle_write_concat_file(IMAGE_DIR + "_color_hist", im_fnames, color_hists)
-  pickle_write_concat_file(IMAGE_DIR + "_first_pixel", im_fnames, first_pixels)
+########## Run Main ############
+t1 = time.time()
+imgs2npy()
+npy2features();
+concat_features();
+t2 = time.time()
+print(t2-t1)
 
 ########## Test load summary data ############
-im_fnames = sorted(glob.glob(IMAGE_DIR + "/*.JPEG"))
-im2vectors(im_fnames);
-concat_vectors(im_fnames);
-concat_fname = IMAGE_DIR + "_first_pixel.p"
+img_fnames = get_img_fnames()
+N = len(img_fnames)
+concat_fname = feature_fname_get('color_hist')
 with open(concat_fname, 'rb') as f:
   data = pickle.load(f)
-
-pdb.set_trace()
 
