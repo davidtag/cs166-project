2902aa77606015aac890cb29b4fb5df82f9804d2
gregbkatz <gregbkatz@gmail.com>
2019-05-27 16:03:12 -0700

    Improve plotting in linear_scan

--- linear_scan.py
+++ linear_scan.py
@@ -10,6 +10,10 @@ import os
 from pylab import get_current_fig_manager
 # from sklearn.metrics.pairwise import rbf_kernel
 import pdb
+import matplotlib.gridspec as gridspec
+
+QUERIES_PER_PLOT = 4
+PLOTS_PER_QUERY = 12
 
 
 class NN:
@@ -18,6 +22,7 @@ class NN:
     self.fnames = fnames
     self.d, self.n = X.shape
     self.q = None
+    self.query_count = 0
 
   def query_idx(self, i):
     q = X[:,i:i+1]
@@ -29,7 +34,9 @@ class NN:
     # print(_all.mean())
     closest = _all.argsort() #[::-1]
     # idx = [closest[0], closest[1], closest[2], closest[3], closest[-2], closest[1]]
-    self.show_query(closest, 3)
+
+    self.query_count += 1
+    self.show_query(closest, PLOTS_PER_QUERY)
 
 
   def get_dist(self):
@@ -50,27 +57,41 @@ class NN:
     fname, _ = os.path.splitext(fname)
     # plt.title(fname)
     plt.axis('off')
+    plt.gca().set_aspect('equal')
 
 
   def show_query(self, closest, N):
-    plt.figure(figsize=(8,6))
-    thismanager = get_current_fig_manager()
-    thismanager.window.wm_geometry("+50+50")
-    for i in range(N):
-      plt.subplot(N,2,2*i+1)
-      self.show_im(closest[i])
-      plt.subplot(N,2,2*i+2)
-      self.show_im(closest[-(i+1)])
 
-    plt.show()
+    cnt = self.query_count % QUERIES_PER_PLOT
+    if cnt == 0:
+      cnt = QUERIES_PER_PLOT
+
+    if cnt == 1:
+      plt.figure(figsize=(15,15))
+      thismanager = get_current_fig_manager()
+      thismanager.window.wm_geometry("+50+50")
+
+    for i in range(N):
+      plt.subplot(QUERIES_PER_PLOT, N, (cnt-1)*N + i+1)
+      self.show_im(closest[i])
+
+      # Show anti-examples
+      # plt.subplot(2,N, N + i+1)
+      # self.show_im(closest[-(i+1)])
+
+    plt.subplots_adjust(wspace=0.001, hspace=0)
+
+    if cnt == QUERIES_PER_PLOT:
+      plt.show()
 
 
 
 # fname = "./imnet-100/color_hist-100.p"
-fname = "./imnet-val/color_hist-100.p"
-fname = "./imnet-val/color_hist-1000.p"
-fname = "./imnet-val/hog-1000.p"
+# fname = "./imnet-val/color_hist-100.p"
+# fname = "./imnet-val/color_hist-1000.p"
+# fname = "./imnet-val/hog-1000.p"
 fname = "./imnet-val/color_hist-5000.p"
+# fname = "./imnet-val/hog-5000.p"
 with open(fname, 'rb') as f:
   data = pickle.load(f)
 
