8aaa7263215c816a0d520b1f90181e1e63aed2c2
gregbkatz <gregbkatz@gmail.com>
2019-06-02 15:01:49 -0700

    Generalized plotting moved into query.py

--- query.py
+++ query.py
@@ -65,7 +65,7 @@ class query:
             self.query_idx(q_index, N_neighbor_candidates, k_report)
             t1 = time.time()
             rank_3_lsh_ip   = self.approx_top_k(refine="innerprod")
-            times[q_index] = time.time() - t1
+            times[q_index] = (time.time() - t1)*1000
             
             #Get Ranks
             self.get_distances()
@@ -210,3 +210,172 @@ class query:
         plt.subplots_adjust(wspace=0.1, hspace=0.1)
         plt.show()
         
+        
+        
+def pareto_frontier(Xs, Ys, maxX = True, maxY = True):
+    myList = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+    p_front = [myList[0]]    
+    for pair in myList[1:]:
+        if maxY: 
+            if pair[1] >= p_front[-1][1]:
+                p_front.append(pair)
+        else:
+            if pair[1] <= p_front[-1][1]:
+                p_front.append(pair)
+    p_frontX = [pair[0] for pair in p_front]
+    p_frontY = [pair[1] for pair in p_front]
+    return p_frontX, p_frontY
+        
+def param_search(data, queries, Ls, bs, es, N_queries):
+    k = 10  #top-k nearest neighbors
+    N_queries = 10
+
+    Ms = []
+    N_data = data.X.shape[1]
+    for e in es:
+        Ms.append(round(2*N_data**(1/(1+e))))
+
+    ts = np.zeros((len(bs), len(es), len(Ls)))
+    ndgcs = np.zeros((len(bs), len(es), len(Ls)))
+    bs_rep = np.zeros((len(bs), len(es), len(Ls)))
+    es_rep = np.zeros((len(bs), len(es), len(Ls)))
+    Ms_rep = np.zeros((len(bs), len(es), len(Ls)))
+    Ls_rep = np.zeros((len(bs), len(es), len(Ls)))
+    print("N_data, N_queries, {}, {}".format(N_data, N_queries))
+    print("Ls,", Ls)
+    print("bs,", bs)
+    print("es,", es)
+    print("Ms,", Ms)
+    print("{:>6}, {:>6}, {:>6}, {:>5}, {:>5}, {:>5}, {:>5}, {:>8}, {:>8}".format(
+        "b_idx", "e_idx", "L_idx", "b", "eps", "M", "L", "t (msec)", "ndgc"))
+
+    for b_idx,b in enumerate(bs):
+        for e_idx,e in enumerate(es):
+            M = Ms[e_idx]
+            t1 = time.time()
+            query_obj = query(data=data, queries_dataset=queries, hash_bits=b, 
+                              permutations=Ms[e_idx])
+    #         print("precomp", time.time() - t1)
+            t1 = time.time()
+            for L_idx,L in enumerate(Ls):
+                t, ndgc = query_obj.time_and_compare(N_queries, N_neighbor_candidates=L, k_report=k)
+                ts[b_idx,e_idx,L_idx] = t
+                ndgcs[b_idx,e_idx,L_idx] = ndgc
+                bs_rep[b_idx, e_idx, L_idx] = b
+                es_rep[b_idx, e_idx, L_idx] = e
+                Ms_rep[b_idx, e_idx, L_idx] = M
+                Ls_rep[b_idx, e_idx, L_idx] = L
+                print("{:6d}, {:6d}, {:6d}, {:5d}, {:5.1f}, {:5d}, {:5d}, {:8.3f}, {:8.3f}".format(
+                    b_idx, e_idx, L_idx, b, e, M, L, t, ndgc))
+    #         print("query", time.time() - t1, (time.time() - t1)/N_queries/len(Ls))
+    
+    out = {"ts": ts, "ndgcs": ndgcs, 
+           "bs_rep": bs_rep,
+           "es_rep": es_rep,
+           "Ms_rep": Ms_rep,
+           "Ls_rep": Ls_rep,
+    }
+    return out
+
+
+def plot_search(results, N_data, N_queries, axlims=None):
+    ts = results["ts"]
+    ndgcs = results["ndgcs"]
+    
+    x = ts.flatten()
+    y = ndgcs.flatten()
+    optX, optY = pareto_frontier(x, y, maxX = False)
+    plt.plot(optX, optY, '-b')
+
+    plt.plot(x, y, 'xb')
+    plt.xlabel('msec')
+    plt.ylabel('ndgc')
+    plt.title('N_data = {}, N_queries = {}'.format(N_data, N_queries))
+    if axlims:
+        plt.axis(axlims)
+    plt.show()
+
+def plot_param_search(results, idx, N_data, N_queries, axlims=None):
+    ts = results["ts"]
+    ndgcs = results["ndgcs"]
+    
+    n,m,l = ts.shape
+    i,j,k = n//2, m//2, l//2
+    
+    if idx == 0:
+        x = ts[:,j,k]
+        y = ndgcs[:,j,k]
+        title_str = "Vary b"
+    elif idx == 1:
+        x = ts[i,:,k]
+        y = ndgcs[i,:,k]
+        title_str = "Vary eps"
+
+    elif idx == 2:
+        x = ts[i,j,:]
+        y = ndgcs[i,j,:]
+        title_str = "Vary L"
+
+    plt.plot(x,y, 'x-')
+    
+    plt.xlabel('msec')
+    plt.ylabel('ndgc')
+    plt.title('N_data = {}, N_queries = {}\n{}'.format(N_data, N_queries, title_str))
+    if axlims:
+        plt.axis(axlims)
+    plt.show()
+
+    
+    if idx == 0:
+        for j in range(m):
+            for k in range(l):
+                plt.plot(ts[:, j, k], ndgcs[:, j, k], 'x-')
+    elif idx == 1:
+        for i in range(n):
+            for k in range(l):
+                plt.plot(ts[i, :, k], ndgcs[i, :, k], 'x-')
+    elif idx == 2:
+        for i in range(n):
+            for j in range(m):
+                plt.plot(ts[i, j, :], ndgcs[i, j, :], 'x-')
+        
+    plt.xlabel('msec')
+    plt.ylabel('ndgc')
+    plt.title('N_data = {}, N_queries = {}\n{}'.format(N_data, N_queries, title_str))
+    plt.axis(axlims)
+    plt.show()
+
+
+    plt.figure(figsize=(5,10))
+    ax1 = plt.subplot(2,1,1)
+    plt.title('N_data = {}, N_queries = {}'.format(N_data, N_queries))
+    ax2 = plt.subplot(2,1,2)
+
+    if idx == 0:
+        x_str = "b"
+        param = results["bs_rep"]
+        for j in range(m):
+            for k in range(l):
+                ax1.plot(param[:,j,k], ts[:, j, k], 'x-')
+                ax2.plot(param[:,j,k], ndgcs[:, j, k], 'x-')               
+    elif idx == 1:
+        x_str = "eps"
+        param = results["es_rep"]
+        for i in range(n):
+            for k in range(l):
+                ax1.plot(param[i,:,k], ts[i, :, k], 'x-')
+                ax2.plot(param[i,:,k], ndgcs[i, :, k], 'x-')    
+    elif idx == 2:
+        x_str = "L"
+        param = results["Ls_rep"]
+        for i in range(n):
+            for j in range(m):
+                ax1.plot(param[i,j,:], ts[i, j, :], 'x-')
+                ax2.plot(param[i,j,:], ndgcs[i, j, :], 'x-')                
+
+    ax1.set_ylabel('msec')
+    ax2.set_ylabel('ndgc')
+    ax2.set_xlabel(x_str)
+#     ax2.set_ylim([0.7, 1.0])
+
+    plt.show()
